5b561e9c0e8a3520f30a0b8aeedfec95226cb1fa
Emon Hossain <emonhossain0044@gmail.com>
2023-04-06 14:36:56 +0600

    updated

--- Live Assignment/assignment3/script.js
+++ Live Assignment/assignment3/script.js
@@ -1,31 +1,22 @@
-function getDate(){
-    const d = new Date();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  
-    const day = days[d.getDay()];
-    const date = d.getDate();
-    const month = months[d.getMonth()];
-    const year = d.getFullYear();
-  
-    return `${day}, ${date}, ${month}, ${year}`;
+function displayTime(){
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var min = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var session = document.getElementById('session');
+
+  if(hrs >= 12){
+      session.innerHTML = 'PM';
+  }else{
+      session.innerHTML = 'AM';
   }
-  document.querySelector(".date").innerHTML = getDate();
-  
-  function getTime(){
-    const d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-  
-    const ampm  = h >= 12 ? "PM" : "AM";
-    h = h % 12 || 12;
-    h = h < 10 ? "0" : h;
-    m = m < 10 ? "0" : m;
-    s = s < 10 ? "0" : s;
-  
-    const time = `${h} : ${m} : ${s} ${ampm}`;
-    document.querySelector(".time").innerHTML = time;
+
+  if(hrs > 12){
+      hrs = hrs - 12;
   }
-  
-  setInterval(getTime, 1000);
+
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 1000);
